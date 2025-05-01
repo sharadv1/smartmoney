26c32e139096e3af292017a3b6379463b66534c1
sharadv1 <sharad.venkat@gmail.com>
2025-05-01 13:54:31 -0500

    updated some things

--- tailwind.config.js
+++ tailwind.config.js
@@ -60,7 +60,8 @@ module.exports = {
         sm: "calc(var(--radius) - 4px)",
       },
       fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
+        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+        mono: ["var(--font-geist-mono)", ...fontFamily.mono],
       },
       keyframes: {
         "accordion-down": {
